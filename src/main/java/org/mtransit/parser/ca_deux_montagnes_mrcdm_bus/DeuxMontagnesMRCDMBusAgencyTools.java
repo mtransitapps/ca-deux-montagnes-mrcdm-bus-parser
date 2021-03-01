bde7f8bd92701da72bd091b3d9cdcd470acffc52
Mathieu Méa <mathieu.mea@gmail.com>
2021-03-01 10:03:18 -0500

    Cleanup

--- src/main/java/org/mtransit/parser/ca_deux_montagnes_mrcdm_bus/DeuxMontagnesMRCDMBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_deux_montagnes_mrcdm_bus/DeuxMontagnesMRCDMBusAgencyTools.java
@@ -1,103 +1,57 @@
 package org.mtransit.parser.ca_deux_montagnes_mrcdm_bus;
 
-import org.mtransit.parser.CleanUtils;
+import org.jetbrains.annotations.NotNull;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.RegexUtils;
 import org.mtransit.parser.DefaultAgencyTools;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
-import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
+import org.mtransit.parser.MTLog;
 import org.mtransit.parser.gtfs.data.GStop;
-import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static org.mtransit.commons.StringUtils.EMPTY;
 
 // https://exo.quebec/en/about/open-data
 // https://exo.quebec/fr/a-propos/donnees-ouvertes
 // https://exo.quebec/xdata/mrc2m/google_transit.zip
 public class DeuxMontagnesMRCDMBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-deux-montagnes-mrcdm-bus-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new DeuxMontagnesMRCDMBusAgencyTools().start(args);
 	}
 
-	private HashSet<String> serviceIds;
-
 	@Override
-	public void start(String[] args) {
-		System.out.printf("\nGenerating MRC2M bus data...");
-		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIds(args, this);
-		super.start(args);
-		System.out.printf("\nGenerating MRC2M bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
+	@NotNull
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIds != null && this.serviceIds.isEmpty();
+	public String getAgencyName() {
+		return "exo Deux-Montagnes (Oka Express)";
 	}
 
-	@Override
-	public boolean excludeCalendar(GCalendar gCalendar) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendar(gCalendar, this.serviceIds);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	@Override
-	public boolean excludeTrip(GTrip gTrip) {
-		if (this.serviceIds != null) {
-			return excludeUselessTrip(gTrip, this.serviceIds);
-		}
-		return super.excludeTrip(gTrip);
-	}
-
+	@NotNull
 	@Override
 	public Integer getAgencyRouteType() {
 		return MAgency.ROUTE_TYPE_BUS;
 	}
 
-	private static final Pattern SECTEUR = Pattern.compile("(secteur[s]? )", Pattern.CASE_INSENSITIVE);
-	private static final String SECTEUR_REPLACEMENT = "";
-
+	@NotNull
 	@Override
-	public String getRouteLongName(GRoute gRoute) {
-		String routeLongName = gRoute.getRouteLongName();
+	public String cleanRouteLongName(@NotNull String routeLongName) {
 		routeLongName = CleanUtils.SAINT.matcher(routeLongName).replaceAll(CleanUtils.SAINT_REPLACEMENT);
-		routeLongName = SECTEUR.matcher(routeLongName).replaceAll(SECTEUR_REPLACEMENT);
 		return CleanUtils.cleanLabel(routeLongName);
 	}
 
 	private static final String AGENCY_COLOR = "1F1F1F"; // DARK GRAY (from GTFS)
 
+	@NotNull
 	@Override
 	public String getAgencyColor() {
 		return AGENCY_COLOR;
-	}
-
-	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		mTrip.setHeadsignString(cleanTripHeadsign(gTrip.getTripHeadsign()), gTrip.getDirectionId());
 	}
 
 	@Override
@@ -105,12 +59,10 @@ public class DeuxMontagnesMRCDMBusAgencyTools extends DefaultAgencyTools {
 		return true;
 	}
 
-	private static final Pattern DIRECTION = Pattern.compile("(direction )", Pattern.CASE_INSENSITIVE);
-	private static final String DIRECTION_REPLACEMENT = "";
-
+	@NotNull
 	@Override
-	public String cleanTripHeadsign(String tripHeadsign) {
-		tripHeadsign = DIRECTION.matcher(tripHeadsign).replaceAll(DIRECTION_REPLACEMENT);
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		tripHeadsign = CleanUtils.cleanStreetTypesFRCA(tripHeadsign);
 		return CleanUtils.cleanLabelFR(tripHeadsign);
 	}
 
@@ -129,20 +81,22 @@ public class DeuxMontagnesMRCDMBusAgencyTools extends DefaultAgencyTools {
 	private static final Pattern AVENUE = Pattern.compile("( avenue)", Pattern.CASE_INSENSITIVE);
 	private static final String AVENUE_REPLACEMENT = " av.";
 
+	@NotNull
 	@Override
-	public String cleanStopName(String gStopName) {
+	public String cleanStopName(@NotNull String gStopName) {
 		gStopName = AVENUE.matcher(gStopName).replaceAll(AVENUE_REPLACEMENT);
-		gStopName = Utils.replaceAll(gStopName, START_WITH_FACES, CleanUtils.SPACE);
-		gStopName = Utils.replaceAll(gStopName, SPACE_FACES, CleanUtils.SPACE);
+		gStopName = RegexUtils.replaceAllNN(gStopName, START_WITH_FACES, CleanUtils.SPACE);
+		gStopName = RegexUtils.replaceAllNN(gStopName, SPACE_FACES, CleanUtils.SPACE);
 		return CleanUtils.cleanLabelFR(gStopName);
 	}
 
 	private static final String ZERO = "0";
 
+	@NotNull
 	@Override
-	public String getStopCode(GStop gStop) {
+	public String getStopCode(@NotNull GStop gStop) {
 		if (ZERO.equals(gStop.getStopCode())) {
-			return null;
+			return EMPTY;
 		}
 		return super.getStopCode(gStop);
 	}
@@ -161,43 +115,44 @@ public class DeuxMontagnesMRCDMBusAgencyTools extends DefaultAgencyTools {
 	private static final String SEU = "SEU";
 
 	@Override
-	public int getStopId(GStop gStop) {
-		String stopCode = getStopCode(gStop);
-		if (stopCode != null && stopCode.length() > 0) {
-			return Integer.valueOf(stopCode); // using stop code as stop ID
+	public int getStopId(@NotNull GStop gStop) {
+		final String stopCode = getStopCode(gStop);
+		if (stopCode.length() > 0) {
+			return Integer.parseInt(stopCode); // using stop code as stop ID
 		}
 		// generating integer stop ID
-		Matcher matcher = DIGITS.matcher(gStop.getStopId());
-		matcher.find();
-		int digits = Integer.parseInt(matcher.group());
-		int stopId;
-		if (gStop.getStopId().startsWith(DMO)) {
-			stopId = 100000;
-		} else if (gStop.getStopId().startsWith(OK)) {
-			stopId = 200000;
-		} else if (gStop.getStopId().startsWith(SPL)) {
-			stopId = 300000;
-		} else if (gStop.getStopId().startsWith(SJL)) {
-			stopId = 400000;
-		} else if (gStop.getStopId().startsWith(SEU)) {
-			stopId = 600000;
-		} else {
-			System.out.println("Stop doesn't have an ID (start with)! " + gStop);
-			System.exit(-1);
-			stopId = -1;
+		//noinspection deprecation
+		final String stopId1 = gStop.getStopId();
+		final Matcher matcher = DIGITS.matcher(stopId1);
+		if (matcher.find()) {
+			final int digits = Integer.parseInt(matcher.group());
+			int stopId;
+			if (stopId1.startsWith(DMO)) {
+				stopId = 100_000;
+			} else if (stopId1.startsWith(OK)) {
+				stopId = 200_000;
+			} else if (stopId1.startsWith(SPL)) {
+				stopId = 300_000;
+			} else if (stopId1.startsWith(SJL)) {
+				stopId = 400_000;
+			} else if (stopId1.startsWith(SEU)) {
+				stopId = 600_000;
+			} else {
+				throw new MTLog.Fatal("Stop doesn't have an ID (start with)! " + gStop);
+			}
+			if (stopId1.endsWith(A)) {
+				stopId += 1000;
+			} else if (stopId1.endsWith(B)) {
+				stopId += 2000;
+			} else if (stopId1.endsWith(C)) {
+				stopId += 3000;
+			} else if (stopId1.endsWith(D)) {
+				stopId += 4000;
+			} else {
+				throw new MTLog.Fatal("Stop doesn't have an ID (end with)! " + gStop);
+			}
+			return stopId + digits;
 		}
-		if (gStop.getStopId().endsWith(A)) {
-			stopId += 1000;
-		} else if (gStop.getStopId().endsWith(B)) {
-			stopId += 2000;
-		} else if (gStop.getStopId().endsWith(C)) {
-			stopId += 3000;
-		} else if (gStop.getStopId().endsWith(D)) {
-			stopId += 4000;
-		} else {
-			System.out.println("Stop doesn't have an ID (end with)! " + gStop);
-			System.exit(-1);
-		}
-		return stopId + digits;
+		throw new MTLog.Fatal("Unexpected stop ID for %s!", gStop);
 	}
 }
